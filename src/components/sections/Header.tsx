b97560edc8607accb1ded5e893b8ca9c48c482ce
NipunShrivats <nipunrawat16@gmail.com>
2024-11-20 12:11:07 +0530

    changes on Nov20, commit no.2

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,11 +1,42 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, AlignJustify } from "lucide-react";
+// import Logo from "@/assets/logosaas.png";
+import Logo1 from "@/assets/falcon1-logo.png";
+import Logo2 from "@/assets/falcon2-logo.png";
+import Link from "next/link";
+
+import Image from "next/image";
+
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
-      <div className="inline-flex gap-1 items-center">
-        <p>Get Started for free</p>
-        <ArrowRight className="h-4 inline-flex justify-center items-center" />
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Get Started for free</p>
+          <ArrowRight className="h-4 inline-flex justify-center items-center" />
+        </div>
       </div>
-    </div>
+      <div className="py-5">
+        <div className="container px-4 md:px-0">
+          <div className="flex justify-between items-center">
+            <Image src={Logo1} alt="" className="w-16 md:w-[80]" />
+            <AlignJustify className="md:hidden border-2" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <Link href="#">About</Link>
+              <Link href="#">Features</Link>
+              <Link href="#">Customers</Link>
+              <Link href="#">Updates</Link>
+              <Link href="#">Help</Link>
+
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                Get for free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,7 +1,22 @@
 export const Hero = () => {
   return (
-    <>
-      <div className="text-rose-600">this is hero</div>
-    </>
+    <section>
+      <div className="container">
+        <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+          version 2.0 is here
+        </div>
+        <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+          Pathway to Productivity
+        </h1>
+        <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+          Celebrate the joy of accomplashimnet with an app designed to track
+          your progress, motivate your efforts, and celebrate your success.
+        </p>
+        <div className="flex gap-1 item-center mt-[30px]">
+          <button>Get For Free</button>
+          <button>Learn More</button>
+        </div>
+      </div>
+    </section>
   );
 };
