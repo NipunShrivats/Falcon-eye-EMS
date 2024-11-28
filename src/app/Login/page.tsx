3a9d756a428e35ff86b887191e228fd66569a341
Nipun Shrivats <nipunrawat16@gmail.com>
2024-11-28 12:08:03 +0530

    changes on 27Nov, commit no.18

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,21 +1,49 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import { Header } from "@/components/sections/Header";
 
 import loginImg from "@/assets/LoginForm/login-Img1.jpg";
-
+import { User, LockKeyhole } from "lucide-react";
 export default function Login() {
   return (
-    <div className="login-background">
-      <div className="login-box">
-        {/* left data */}
-        <div className="left-side">
-          <Image src={loginImg} alt="loginImg" />
-        </div>
-        {/* right data */}
-        <div className="right-side">
-          {/* <form action=""></form> */}
-          right
+    <div className="bg-gradient-to-r from-[#4c6ef5] to-[#d290f1] h-[100vh]">
+      <Header />
+      <div className="flex items-center justify-center flex-col h-[82vh]">
+        <div className="flex items-center justify-between rounded-xl">
+          <div className="">
+            <Image
+              src={loginImg}
+              alt="loginImg"
+              className="w-[30rem] h-auto rounded-l-xl"
+            />
+          </div>
+          <div className="border-2 h-full rounded-r-xl w-[20rem] flex flex-col items-center justify-center gap-8">
+            <h3>USER LOGIN</h3>
+            <form
+              action=""
+              className="flex flex-col items-center justify-center gap-2"
+            >
+              <div className="input-box">
+                <User />
+                <input
+                  type="text"
+                  placeholder="user id"
+                  className="login-input"
+                />
+              </div>
+              <div className="input-box">
+                <LockKeyhole />
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="login-input"
+                />
+              </div>
+              <p>Forgot password?</p>
+              <button>Login</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
