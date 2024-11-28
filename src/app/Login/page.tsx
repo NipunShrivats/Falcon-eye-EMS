283ff1770e85c763403355bd5a2cb816380118f9
Nipun Shrivats <nipunrawat16@gmail.com>
2024-11-28 14:01:14 +0530

    changes on 27Nov, commit no.19

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -3,29 +3,33 @@ import React from "react";
 import Image from "next/image";
 import { Header } from "@/components/sections/Header";
 
-import loginImg from "@/assets/LoginForm/login-Img1.jpg";
+import loginImg from "@/assets/LoginForm/login-Img6.jpg";
 import { User, LockKeyhole } from "lucide-react";
 export default function Login() {
   return (
-    <div className="bg-gradient-to-r from-[#4c6ef5] to-[#d290f1] h-[100vh]">
+    // <div className="bg-gradient-to-r from-[#4c6ef5] via-[#956eee] to-[#d290f1] h-[100vh]">
+    <div className="bg-[url('@/assets/LoginForm/login-Img6.jpg')] bg-center bg-no-repeat bg-cover ">
       <Header />
       <div className="flex items-center justify-center flex-col h-[82vh]">
-        <div className="flex items-center justify-between rounded-xl">
+        <div className="flex items-center justify-between rounded-xl shadow-2xl">
           <div className="">
             <Image
               src={loginImg}
               alt="loginImg"
-              className="w-[30rem] h-auto rounded-l-xl"
+              className="w-[40rem] h-auto rounded-l-xl"
             />
+            {/* <div className="">hi</div> */}
           </div>
-          <div className="border-2 h-full rounded-r-xl w-[20rem] flex flex-col items-center justify-center gap-8">
-            <h3>USER LOGIN</h3>
+          <div className="bg-white h-full rounded-r-xl w-[20rem] flex flex-col items-center justify-center gap-4 ">
+            <h3 className="font-sans font-bold text-black text-xl">
+              USER LOGIN
+            </h3>
             <form
               action=""
-              className="flex flex-col items-center justify-center gap-2"
+              className="flex flex-col items-center justify-center gap-3 "
             >
               <div className="input-box">
-                <User />
+                <User className="w-[1.3rem]" />
                 <input
                   type="text"
                   placeholder="user id"
@@ -33,15 +37,21 @@ export default function Login() {
                 />
               </div>
               <div className="input-box">
-                <LockKeyhole />
+                <LockKeyhole className="w-[1.3rem]" />
                 <input
                   type="password"
                   placeholder="password"
                   className="login-input"
                 />
               </div>
-              <p>Forgot password?</p>
-              <button>Login</button>
+              <p className="text-sm">
+                Forgot password? <span>Contact HR!</span>
+              </p>
+              <div className="">
+                <button className="bg-black text-white font-medium border border-[rgba(0,0,0,0.3)] py-2 px-14 rounded-[500rem] hover:bg-slate-800 transition ease-in-out duration-500">
+                  Login
+                </button>
+              </div>
             </form>
           </div>
         </div>
