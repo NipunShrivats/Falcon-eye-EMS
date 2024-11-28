3d6757ba67ed493b70081bc3098226fd9b224102
Nipun Shrivats <nipunrawat16@gmail.com>
2024-11-28 10:35:13 +0530

    changes on 27Nov, commit no.17

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,6 +1,23 @@
 "use client";
 import React from "react";
+import Image from "next/image";
+
+import loginImg from "@/assets/LoginForm/login-Img1.jpg";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-background">
+      <div className="login-box">
+        {/* left data */}
+        <div className="left-side">
+          <Image src={loginImg} alt="loginImg" />
+        </div>
+        {/* right data */}
+        <div className="right-side">
+          {/* <form action=""></form> */}
+          right
+        </div>
+      </div>
+    </div>
+  );
 }
